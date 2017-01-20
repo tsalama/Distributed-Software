b6ce849bb46f5f1438e22b4f3c783749eb8aed7b
Tarek Salama <tsalama@asu.edu>
2017-01-19 19:44:04 -0700

    SimpleWebService

---
+++ Activity1/Service1.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Activity1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public int sumOfDigits(int value)
+        {
+            string str = value.ToString();
+            int sum = 0;
+
+            for (int i = 0; i < str.Length; i++)
+                sum += (int)char.GetNumericValue(str[i]);
+
+            return sum;
+        }
+
+        public int numVowels(string str)
+        {
+            int vowels = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'
+                    || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                    vowels++;
+            }
+
+            return vowels;
+        }
+    }
+}
---
+++ Activity1Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity1Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tWelcome To Web Service Demo\n");
+            Console.WriteLine("Select the method you need to invoke\n");
+            Console.WriteLine("1. Sum of digits\n2. Vowel count\n3. Exit\n");
+            Console.WriteLine("Enter your option: ");
+
+            string option = Console.ReadLine();
+            Activity1ServiceReference.Service1Client proxy = new Activity1ServiceReference.Service1Client();
+
+            if (option.Equals("1"))
+            {
+                Console.WriteLine("Enter an integer: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sum of digits = " + proxy.sumOfDigits(num));
+            }
+            else if (option.Equals("2"))
+            {
+                Console.WriteLine("Enter a string: ");
+                string str = Console.ReadLine();
+                Console.WriteLine("Number of Vowels = " + proxy.numVowels(str));
+            }
+            else if (option.Equals("3"))
+                Environment.Exit(0);
+            else
+                Console.WriteLine("Invalid Input.");
+        }
+    }
+}

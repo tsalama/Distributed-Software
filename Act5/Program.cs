3c616ed5eee95df87e67de679ada26e91771f069
Tarek Salama <tsalama@asu.edu>
2017-02-23 16:04:50 -0700

    Act4

---
+++ Act5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Act5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Enter String: ");
+                // read a string for analysis
+                string str = Console.ReadLine();
+
+                // create threads
+                ThreadClass tc = new ThreadClass();
+                Thread t1 = new Thread(() => tc.digitCount(str));
+                Thread t2 = new Thread(() => tc.upperCount(str));
+                Thread t3 = new Thread(() => tc.isPalindrome(str));
+                // start each thread
+                t1.Start();
+                t2.Start();
+                t3.Start();
+                // wait until all the threads are done
+                t1.Join(); 
+                t2.Join(); 
+                t3.Join(); 
+                // Display the string analysis results
+                Console.WriteLine($"\n# Digits: {tc.getDigitCount()} \n# Uppers: {tc.getUpperCount()} \nisPalindrome: {tc.isPalin()}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Act5/ThreadClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Act5
+{
+    class ThreadClass
+    {
+        int numDigits = 0;
+        int numUppers = 0;
+        bool isPal = true;
+
+        public void digitCount(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsDigit(str[i]))
+                    numDigits++;
+            }
+        }
+        public void upperCount(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsUpper(str[i]))
+                    numUppers++;
+            }
+        }
+        public void isPalindrome(string str)
+        {
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                char ch1 = Char.ToLower(str[i]);
+                char ch2 = Char.ToLower(str[str.Length - 1 - i]);
+
+                if (ch1 != ch2)
+                    isPal = false;
+            }
+        }
+
+        public int getDigitCount()
+        {
+            return numDigits;
+        }
+
+        public int getUpperCount()
+        {
+            return numUppers;
+        }
+
+        public bool isPalin()
+        {
+            return isPal;
+        }
+
+    }
+}
